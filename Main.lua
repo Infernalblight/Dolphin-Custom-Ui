346d46e9fa4b5fb5a154f353ea06a2f4eb872b40
Infernalblight <129289821+Infernalblight@users.noreply.github.com>
2024-11-24 12:20:13 +0100

    Update Main.lua

--- Main.lua
+++ Main.lua
@@ -25,10 +25,15 @@ _G.CustomTheme = {
 
 local LocalPlayerTab = Library:CreateTab("Local-Player", "", true)
 
+-- State to control if WalkSpeed and JumpPower should repeat
 local walkSpeedActive = false
-local lastWalkSpeed = 16
+local jumpPowerActive = false
+local lastWalkSpeed = 16  -- Default WalkSpeed value
+local lastJumpPower = 50  -- Default JumpPower value
 
+-- Create the slider for WalkSpeed
 LocalPlayerTab:CreateSlider("WalkSpeed", 0, 600, function(arg)
+    -- Update WalkSpeed only if the repeat feature is active
     if walkSpeedActive then
         print("WalkSpeed is set to:", arg)
         local player = game.Players.LocalPlayer
@@ -37,25 +42,69 @@ LocalPlayerTab:CreateSlider("WalkSpeed", 0, 600, function(arg)
             lastWalkSpeed = arg
         end
     else
+        -- If repeat is not active, just update the last WalkSpeed value
         lastWalkSpeed = arg
     end
 end)
 
+-- Create the toggle for enabling/disabling WalkSpeed repeating
 LocalPlayerTab:CreateToggle("Enable WalkSpeed Repeat", function(arg)
     if arg then
+        -- Enable WalkSpeed repeat and start repeating the current WalkSpeed value
         walkSpeedActive = true
         print("WalkSpeed repeat is now enabled.")
         
+        -- Set WalkSpeed to the last slider value (if available)
         local player = game.Players.LocalPlayer
         if player.Character and player.Character:FindFirstChild("Humanoid") then
             player.Character.Humanoid.WalkSpeed = lastWalkSpeed
         end
     else
+        -- Disable WalkSpeed repeat and set WalkSpeed back to 16
         walkSpeedActive = false
         local player = game.Players.LocalPlayer
         if player.Character and player.Character:FindFirstChild("Humanoid") then
             player.Character.Humanoid.WalkSpeed = 16
         end
         print("WalkSpeed repeat is now disabled, WalkSpeed set to 16.")
+    end
+end)
+
+-- Create the slider for JumpPower
+LocalPlayerTab:CreateSlider("JumpPower", 0, 200, function(arg)
+    -- Update JumpPower only if the repeat feature is active
+    if jumpPowerActive then
+        print("JumpPower is set to:", arg)
+        local player = game.Players.LocalPlayer
+        if player.Character and player.Character:FindFirstChild("Humanoid") then
+            player.Character.Humanoid.JumpPower = arg
+            lastJumpPower = arg
+        end
+    else
+        -- If repeat is not active, just update the last JumpPower value
+        lastJumpPower = arg
+    end
+end)
+
+-- Create the toggle for enabling/disabling JumpPower repeating
+LocalPlayerTab:CreateToggle("Enable JumpPower Repeat", function(arg)
+    if arg then
+        -- Enable JumpPower repeat and start repeating the current JumpPower value
+        jumpPowerActive = true
+        print("JumpPower repeat is now enabled.")
+        
+        -- Set JumpPower to the last slider value (if available)
+        local player = game.Players.LocalPlayer
+        if player.Character and player.Character:FindFirstChild("Humanoid") then
+            player.Character.Humanoid.JumpPower = lastJumpPower
+        end
+    else
+        -- Disable JumpPower repeat and set JumpPower back to default value (50)
+        jumpPowerActive = false
+        local player = game.Players.LocalPlayer
+        if player.Character and player.Character:FindFirstChild("Humanoid") then
+            player.Character.Humanoid.JumpPower = 50
+        end
+        print("JumpPower repeat is now disabled, JumpPower set to 50.")
     end
 end)
